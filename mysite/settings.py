b9d279f14b8a3b2b1cf4a5c9bff58e5c3480a882
bonnie xu <xinmiao.xu@nyu.edu>
2024-03-07 22:00:04 -0500

    test3

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-w!ze&m91#l$lj)hj#p9gc3dz^r&8myw%6im3yv!%bfkvg$4hjy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['swe1-app-dev3.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ["swe1-app-dev.us-east-1.elasticbeanstalk.com"]
 # ALLOWED_HOSTS = ['*']
 
 
